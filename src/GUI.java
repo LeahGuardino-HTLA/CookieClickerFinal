7dc7cc869f95c5f31b2b59012ee4b8380651b803
LeahGuardino <Leah.Guardino@ht-la.org>
2026-06-09 11:31:48 -0700

    Final product (button image is still not loading)

--- src/GUI.java
+++ src/GUI.java
@@ -49,10 +49,6 @@ public class GUI implements ActionListener {
         panel3 = new JPanel();
         panel4 = new JPanel();
 
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new BorderLayout());
-
-
 
         label = new JLabel("No baflams brotato");
         cost1 = new JLabel("UPGRADE 1 COST: " + baflams.upgrade1Cost);
@@ -67,26 +63,21 @@ public class GUI implements ActionListener {
         cost10 = new JLabel("UPGRADE 10 COST: " + baflams.upgrade10Cost);
         cost11 = new JLabel("UPGRADE 11 COST: " + baflams.upgrade11Cost);
 
-
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Arial",Font.BOLD,24));
-
-        topPanel.add(label, BorderLayout.NORTH);
-        topPanel.add(baflamButton, BorderLayout.CENTER);
+        baflamButton = new JButton(new ImageIcon("C:\\Users\\LeahGuardino\\Desktop\\forehead.png"));
+        baflamButton.setBackground(new Color(242,231,196));
+            //IMAGE PLEASE WORK BRO
 
 
+
+        //it's actually farting out so I give up on the north, south, east, west, center thing
 
 
         JPanel upgradesPanel = new JPanel();
-        upgradesPanel.setLayout(new GridLayout(11,2,10,10));
-        upgradesPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        //upgradesPanel.setLayout(new GridLayout(11, 2, 10, 10));
+        //upgradesPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 
-
-        //baflamButton = new JButton(new ImageIcon("C:\\Users\\LeahGuardino\\Desktop\\Farts.png"));
-
-
-        baflamButton = new JButton(new ImageIcon("yo"));
+        //baflamButton = new JButton(new ImageIcon("yo"));
         upgradeButton1 = new JButton("PLUS 1");
         upgradeButton2 = new JButton("LOCKED IN");
         upgradeButton3 = new JButton("PLUS 5");
@@ -99,7 +90,6 @@ public class GUI implements ActionListener {
         upgradeButton10 = new JButton("PLUS 300");
         upgradeButton11 = new JButton("PLUS 500");
 
-        baflamButton.setPreferredSize(new Dimension(200,200));
 
         baflamButton.addActionListener(this);
 
@@ -110,8 +100,8 @@ public class GUI implements ActionListener {
             baflams.buyUpgrade2();
         });
         upgradeButton3.addActionListener(e -> {
-                    baflams.buyUpgrade3();
-                });
+            baflams.buyUpgrade3();
+        });
         upgradeButton4.addActionListener(e -> {
             baflams.buyUpgrade4();
         });
@@ -134,119 +124,106 @@ public class GUI implements ActionListener {
             baflams.buyUpgrade10();
         });
         upgradeButton11.addActionListener(e -> {
-                    baflams.buyUpgrade11();
+            baflams.buyUpgrade11();
         });
 
 
-            guiUpdate = new ActionListener() {
+        guiUpdate = new ActionListener() {
 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    label.setText("Baflams: " + baflams.baflamCounter); //only piece of code that does anything, everything else yaps
-                    cost1.setText("UPGRADE COST: " + baflams.upgrade1Cost);
-
-                    cost1.setHorizontalAlignment(JLabel.CENTER);
-
-                    cost2.setText("UPGRADE COST: " + baflams.upgrade2Cost);
-                    cost3.setText("UPGRADE COST: " + baflams.upgrade3Cost);
-                    cost4.setText("UPGRADE COST: " + baflams.upgrade4Cost);
-                    cost5.setText("UPGRADE COST: " + baflams.upgrade5Cost);
-                    cost6.setText("UPGRADE COST: " + baflams.upgrade6Cost);
-                    cost7.setText("UPGRADE COST: " + baflams.upgrade7Cost);
-                    cost8.setText("UPGRADE COST: " + baflams.upgrade8Cost);
-                    cost9.setText("UPGRADE COST: " + baflams.upgrade9Cost);
-                    cost10.setText("UPGRADE COST: " + baflams.upgrade10Cost);
-                    cost11.setText("UPGRADE COST: " + baflams.upgrade11Cost);
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Baflams: " + baflams.baflamCounter); //only piece of code that does anything, everything else yaps
+                cost1.setText("UPGRADE COST: " + baflams.upgrade1Cost);
 
 
-                }
-            };
-            updateTimer = new Timer(500, guiUpdate); //delay, the thing to do
-            updateTimer.start();
+                cost2.setText("UPGRADE COST: " + baflams.upgrade2Cost);
+                cost3.setText("UPGRADE COST: " + baflams.upgrade3Cost);
+                cost4.setText("UPGRADE COST: " + baflams.upgrade4Cost);
+                cost5.setText("UPGRADE COST: " + baflams.upgrade5Cost);
+                cost6.setText("UPGRADE COST: " + baflams.upgrade6Cost);
+                cost7.setText("UPGRADE COST: " + baflams.upgrade7Cost);
+                cost8.setText("UPGRADE COST: " + baflams.upgrade8Cost);
+                cost9.setText("UPGRADE COST: " + baflams.upgrade9Cost);
+                cost10.setText("UPGRADE COST: " + baflams.upgrade10Cost);
+                cost11.setText("UPGRADE COST: " + baflams.upgrade11Cost);
 
 
-            //PROPERTIES
-            panel.setSize(400, 100);
-            panel.setLayout(new GridLayout(3, 3));
-            //first need to add the label to the panel
-            panel.add(baflamButton);
-            panel.add(upgradeButton1);
-            panel.add(upgradeButton2);
-            panel.add(label); //each element needs to be added to our panel after creation
-            panel.add(cost1);
-            panel.add(cost2);
-            panel.setBackground(Color.LIGHT_GRAY); //CHANGES COLOR
+            }
+        };
+        updateTimer = new Timer(500, guiUpdate); //delay, the thing to do
+        updateTimer.start();
 
 
-            panel2.setSize(400, 100);
-            panel2.setLayout(new GridLayout(3,3));
-            panel2.add(upgradeButton3);
-            panel2.add(upgradeButton4);
-            panel2.add(upgradeButton5);
-            panel2.add(cost3);
-            panel2.add(cost4);
-            panel2.add(cost5);
-            panel2.setBackground(Color.cyan);
+        //PROPERTIES
+
+        panel.setLayout(new GridLayout(2, 3));
+        //first need to add the label to the panel
+        panel.add(baflamButton);
+        panel.add(upgradeButton1);
+        panel.add(upgradeButton2);
+        panel.add(label); //each element needs to be added to our panel after creation
+        panel.add(cost1);
+        panel.add(cost2);
+        panel.setBackground(Color.white); //CHANGES COLOR
+        panel.setSize(400, 100);
+
+        frame.add(panel);
 
 
-            panel3.setSize(400, 100);
-            panel3.setLayout(new GridLayout(3,3));
-            panel3.add(upgradeButton6);
-            panel3.add(upgradeButton7);
-            panel3.add(upgradeButton8);
-            panel3.add(cost6);
-            panel3.add(cost7);
-            panel3.add(cost8);
-            panel3.setBackground(Color.green);
+        panel2.setLayout(new GridLayout(2, 3));
+        panel2.add(upgradeButton3);
+        panel2.add(upgradeButton4);
+        panel2.add(upgradeButton5);
+        panel2.add(cost3);
+        panel2.add(cost4);
+        panel2.add(cost5);
+        panel2.setBackground(Color.white);
+        panel2.setSize(400, 400);
+        frame.add(panel2);
 
 
-            panel4.setSize(400, 100);
-            panel4.setLayout(new GridLayout(3,3));
-            panel4.add(upgradeButton9);
-            panel4.add(upgradeButton10);
-            panel4.add(upgradeButton11);
-            panel4.add(cost9);
-            panel4.add(cost10);
-            panel4.add(cost11);
-            panel4.setBackground(Color.YELLOW);
+        panel3.setLayout(new GridLayout(2, 3));
+        panel3.add(upgradeButton6);
+        panel3.add(upgradeButton7);
+        panel3.add(upgradeButton8);
+        panel3.add(cost6);
+        panel3.add(cost7);
+        panel3.add(cost8);
+        panel3.setBackground(Color.white);
+        panel3.setSize(400, 400);
+        frame.add(panel3);
 
 
-            frame.setLayout(new BorderLayout());
-
-            frame.add(topPanel, BorderLayout.NORTH);
-            frame.add(upgradesPanel, BorderLayout.CENTER);
-
-
-            //next, we add the panel to the frame
-            frame.add(panel);
-            frame.add(panel2);
-            frame.add(panel3);
-            frame.add(panel4);
-
+        panel4.setLayout(new GridLayout(2, 3));
+        panel4.add(upgradeButton9);
+        panel4.add(upgradeButton10);
+        panel4.add(upgradeButton11);
+        panel4.add(cost9);
+        panel4.add(cost10);
+        panel4.add(cost11);
+        panel4.setBackground(Color.white);
+        panel4.setSize(400, 400);
+        frame.add(panel4);
 
 
-            frame.setSize(1000, 1000);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-        }
+        //frame.setLayout(new BorderLayout());
+
+        //frame.add(topPanel, BorderLayout.NORTH);
+        //frame.add(upgradesPanel, BorderLayout.CENTER);
 
 
-        @Override
-        public void actionPerformed (ActionEvent e){
-            baflams.click();
-            label.setText("Baflams: " + baflams.baflamCounter);
-
-
-        }
+        frame.setSize(1000, 700);
+        frame.setLayout(new GridLayout(2, 2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
-/*
-TODO:
-change button colors
-Add more upgrades (you need to change the grid)
-~5-10
-They need to be unique and do different things
 
-bonus points
-change your button to cookie images or some other image
- */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        baflams.click();
+        label.setText("Baflams: " + baflams.baflamCounter);
+
+
+    }
+}
